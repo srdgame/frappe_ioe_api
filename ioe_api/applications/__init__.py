1632cff509dee28e31e0d11e1d0c902974650485
Dirk Chang <dirk@kooiot.com>
2019-03-28 18:12:18 +0800

    get doc fixed

--- ioe_api/applications/__init__.py
+++ ioe_api/applications/__init__.py
@@ -29,7 +29,7 @@ def list():
 		apps = []
 		filters = {"owner": frappe.session.user}
 		for d in frappe.get_all("IOT Application", "name", filters=filters, order_by="modified desc"):
-			apps.append(as_dict(frappe.get_doc("IOT Application", d.name, keep_owner=True)))
+			apps.append(as_dict(frappe.get_doc("IOT Application", d.name), keep_owner=True))
 
 		frappe.response.update({
 			"ok": True,
