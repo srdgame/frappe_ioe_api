70dd0e4064019857a089c769235ae278c5249697
Dirk Chang <dirk@kooiot.com>
2019-12-30 11:16:23 +0800

    Copy the implementation and do it ourself.

--- ioe_api/store/tags.py
+++ ioe_api/store/tags.py
@@ -7,7 +7,7 @@
 
 from __future__ import unicode_literals
 import frappe
-from ioe_api.helper import valid_auth_code, list_tags
+from ioe_api.helper import valid_auth_code, list_tags, get_post_json_data
 from frappe.desk.reportview import get_sidebar_stats
 
 
@@ -21,14 +21,33 @@ def test():
 
 
 @frappe.whitelist(allow_guest=True)
-def list():
+def list(filters=[]):
 	try:
 		valid_auth_code()
-		data = get_sidebar_stats(stats="_user_tags", doctype='IOT Application')
+		# data = get_sidebar_stats(stats="_user_tags", doctype='IOT Application')
+		#
+		# frappe.response.update({
+		# 	"ok": True,
+		# 	"data": data.get('stats').get('_user_tags')
+		# })
+
+		_user_tags = []
+		if frappe.request and frappe.request.method == "POST":
+			data = get_post_json_data()
+			filters = data.get("filters") or []
+
+		for tag in frappe.get_all("Tag Link", filters={"document_type": 'IOT Application'}, fields=["tag"]):
+			tag_filters = []
+			tag_filters.extend(filters)
+			tag_filters.extend([['Tag Link', 'tag', '=', tag.tag]])
+
+			count = frappe.get_all('IOT Application', filters=tag_filters, fields=["count(*)"])
+			if count[0].get("count(*)") > 0:
+				_user_tags.append([tag.tag, count[0].get("count(*)")])
 
 		frappe.response.update({
 			"ok": True,
-			"data": data.get('stats').get('_user_tags')
+			"data": _user_tags
 		})
 	except Exception as ex:
 		frappe.response.update({
