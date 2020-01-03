93e35a0fc76131c1ea7d99aa53e9b7c56f412d2a
Dirk Chang <dirk@kooiot.com>
2020-01-03 11:55:50 +0800

    Support auth code for wps_url

--- ioe_api/wps/__init__.py
+++ ioe_api/wps/__init__.py
@@ -8,6 +8,7 @@
 from __future__ import unicode_literals
 import frappe
 from .helper import valid_weboffice_token, get_signature
+from ioe_api.helper import valid_auth_code
 
 
 WPS_APPID = "a7bda3f18c394a967269c15cbd7f224e"
@@ -23,8 +24,9 @@ def test():
 	})
 
 
-@frappe.whitelist()
+@frappe.whitelist(allow_guest=True)
 def wps_url(conf, version, version_new):
+	valid_auth_code()
 	return get_signature(WPS_APPID, WPS_APPKEY, frappe.session.user, frappe.session.sid, conf, version, version_new)
 
 
